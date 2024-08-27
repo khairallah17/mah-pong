c1fadce7702bc18829fee8c611d66a2ca74ddf70
El mehdi Agoumi <eagoumi@e3r3p12.1337.ma>
2024-08-27 22:01:47 +0100

    auto-commit

--- auth_front/src/App.js
+++ auth_front/src/App.js
@@ -4,25 +4,7 @@ import "./App.css"
 const App = () => {
   return (
     <div className="App">
-      <div className='container-form'>
-        <h1 className='register'>Register</h1>
-        {/* <form>
-          <p>Username</p>
-          <input type="text"></input>
-          <p>Email</p>
-          <input type='email'></input>
-          <p>Password</p>
-          <input type='password'></input>
-          <p>Confirm Password</p>
-          <input type='password'></input>
-          <div className='signup-botton'>
-            <input type="button" value={"SignUp"}></input>
-          </div>
-        </form> */}
-      </div>
-      <div className='container-pic'>
-
-      </div>
+      <h1 className="login" >Login</h1>
     </div>
   )
 }
