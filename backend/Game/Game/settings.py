1d9da5557ed72912ffa2d8145252e6c0038461e9
ayoubms8 <ayoub.lemsafi10@gmail.com>
2024-10-28 13:23:12 +0100

    makeing game microservice work with postgresql database

--- backend/Game/Game/settings.py
+++ backend/Game/Game/settings.py
@@ -80,8 +80,12 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'passwd',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
