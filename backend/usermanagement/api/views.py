f88a83f1136cc9405c25d2011a08988a56f54e38
ayoubms8 <ayoub.lemsafi10@gmail.com>
2024-11-02 09:40:26 +0100

    env vars for views.py

--- backend/usermanagement/api/views.py
+++ backend/usermanagement/api/views.py
@@ -27,6 +27,12 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .token_reset_passwordd import account_activation_token
 from django.core.mail import send_mail, EmailMessage
+import os
+
+GCLIENT_ID = os.getenv('GCLIENT_ID')
+GCLIENT_SECRET = os.getenv('GCLIENT_SECRET')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 # Create your views here.
 
