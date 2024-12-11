f0865d74de7ad8ec7616ab040657a180eb5fb834
ayoubms8 <ayoub.lemsafi10@gmail.com>
2024-12-11 16:02:13 +0100

    fetching available tournaments in Tournaments home page

--- backend/Game/Game/urls.py
+++ backend/Game/Game/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Game.Match.views import TournamentList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/tournaments', TournamentList.as_view(), name='tournament-list'),
 ]
--- frontend/src/pages/tournament/tournamentHome.jsx
+++ frontend/src/pages/tournament/tournamentHome.jsx
@@ -1,24 +1,45 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const TournamentHome = () => {
     const navigate = useNavigate();
     const [tournamentCode, setTournamentCode] = useState("");
+    const [tournaments, setTournaments] = useState([]);
 
+    useEffect(() => {
+        const fetchTournaments = async () => {
+            try {
+                const response = await fetch("/api/tournaments");
+                const data = await response.json();
+                setTournaments(data);
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchTournaments();
+    }, [tournaments]);
     return (
         // choose 2d or 3d then choose create or join with code
 
         // create with code
         <div>
             <input placeholder="Tournament code"
-            value={tournamentCode}
-            onChange={(e) => setTournamentCode(e.target.value)}>
+                value={tournamentCode}
+                onChange={(e) => setTournamentCode(e.target.value)}>
             </input>
             <button onClick={() => navigate(`/tournament?code=${tournamentCode}`)}>Join</button>
             <button onClick={() => navigate("/tournament")}>Create</button>
-        <h1>Tournament Home</h1>
+            <h1>Tournament Home</h1>
+            <ul>
+                {tournaments.map((tournament) => (
+                    <li key={tournament.code}>{tournament.status}</li>
+                ))}
+            </ul>
         </div>
     );
-    }
+}
 
 export default TournamentHome;
