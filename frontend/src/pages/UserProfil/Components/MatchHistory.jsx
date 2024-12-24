9acc0cf078bbf278e6dbbaf0fdc8ec297d41b114
El mehdi Agoumi <eagoumi@e2r10p1.1337.ma>
2024-12-24 17:26:54 +0100

    auto-commit

--- frontend/src/pages/UserProfil/Components/MatchHistory.jsx
+++ frontend/src/pages/UserProfil/Components/MatchHistory.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const MatchHistory = () => {
+const MatchHistory = ({ UserPlay }) => {
   const matches = [
-    { id: 1, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '2-5', result: 'LOST', time: '13:37s' },
-    { id: 2, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '10-5', result: 'WON', time: '13:37s' },
-    { id: 3, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '2-5', result: 'LOST', time: '13:37s' },
-    { id: 4, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '10-5', result: 'WON', time: '13:37s' },
-    { id: 5, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '2-5', result: 'LOST', time: '13:37s' },
-    { id: 6, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '10-5', result: 'WON', time: '13:37s' },
-    { id: 7, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '10-5', result: 'WON', time: '13:37s' },
+    { id: 1, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '2', result: 'LOST', time: '13:37s' },
+    { id: 2, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '10', result: 'WON', time: '13:37s' },
+    { id: 3, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '2', result: 'LOST', time: '13:37s' },
+    { id: 4, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'WON', time: '13:37s' },
+    { id: 5, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'LOST', time: '13:37s' },
+    { id: 6, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'WON', time: '13:37s' },
+    { id: 7, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '7', result: 'WON', time: '13:37s' },
+    { id: 8, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'WON', time: '13:37s' },
+    { id: 9, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'LOST', time: '13:37s' },
+    { id: 10, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'LOST', time: '13:37s' },
+    { id: 11, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '5', result: 'WON', time: '13:37s' },
+    { id: 12, date: '23:32, Wed, Dec 6', player1: 'ven', player2: 'mohammed', score: '7', result: 'WON', time: '13:37s' },
   ];
+  // const fetchdata = async () => {
+  //   url = UserPlay
+  // }
 
   const summary = {
     wins: 4,
@@ -23,38 +31,43 @@ const MatchHistory = () => {
         <div className="flex items-center gap-2">
           <h2 className="text-white text-lg font-semibold">Match History</h2>
         </div>
+        <div className="flex justify-center bg-[#393434] w-[152px] h-[50px] items-center">
+          <p className="text-green-400 font-bold text-lg">{summary.wins}W</p>
+          <p className="font-bold m-1">-</p>
+          <p className="text-red-400 font-bold text-lg">{summary.losses}L</p>
+          <p className="text-[#99ABBF] font-bold text-lg">&nbsp;({summary.percentage}%)</p>
+        </div>
       </div>
 
       {/* <div className="space-y-2"> */}
         {matches.map((match) => (
-          <div key={match.id} className="relative">
-            <div className="gap-4 justify-end">
-                <span className={`text-sm font-medium ${
+          <div key={match.id} className="mt-2.5 relative">
+            <div className="flex items-center justify-between">
+              <div className="" >{match.date}</div>
+                <span className={`w-16 h-6 rounded-lg justify-end text-sm font-bold ${
                   match.result === 'WON'
-                    ? 'text-green-400'
-                    : 'text-red-400'
+                    ? 'bg-green-400 text-white-400'
+                    : 'bg-red-400 text-white-400'
                 }`}>
                   {match.result}
                 </span>
             </div>
-            <div className={`p-3 items-center justify-between border-b ${
+              <div className={`flex items-center justify-center gap-3 border-b ${
                   match.result === 'WON'
                     ? 'border-green-400'
                     : 'border-red-400'}`}>
-              <div className="flex items-center justify-center gap-3">
                 <span className="text-white">{match.player1}</span>
-                <div className="w-8 h-8 bg-gray-700 rounded-full"></div>
-                <div className={`px-3 py-1 rounded ${
-                  match.result === 'WON' 
-                    ? 'bg-green-900/90 text-green-400' 
-                    : 'bg-red-900/90 text-red-400'
-                }`}>
-                  {match.score}
+                <div className="w-8 h-8 bg-gray-700 rounded-full font-bold"></div>
+                <div className={`flex justify-center content-center w-16 h-6 rounded m-2 ${
+                    match.result === 'WON' 
+                      ? 'bg-green-900/90 text-white-400' 
+                      : 'bg-red-900/90 text-white-400'
+                  }`}>
+                    {match.score} <p className="font-bold ">-</p> {match.score}
                 </div>
                 <div className="w-8 h-8 bg-gray-700 rounded-full"></div>
                 <span className="text-white">{match.player2}</span>
               </div>
-            </div>
           </div>
         ))}
       {/* </div> */}
