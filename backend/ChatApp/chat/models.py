d4c82dcf43ea9088741d040f95e004bbba96402c
Hamza Salam <hasalam@e2r2p14.1337.ma>
2024-12-27 00:57:48 +0100

    backend changes

--- backend/ChatApp/chat/models.py
+++ backend/ChatApp/chat/models.py
@@ -11,7 +11,7 @@ class Chat(models.Model):
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    chat = models.ForeignKey(Chat, on_delete=models,CASCADE, related_name='messages', null=True, blank=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages', null=True, blank=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     seen = models.BooleanField(default=False)
